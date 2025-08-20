3b90d31b1d72475e1580877a8f492bb21eed63ce
rokoperki <roko.perisic@gmail.com>
2025-08-20 14:19:07 +0200

    renamed assets

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,7 +1,7 @@
 export { default as LogotypeSvg } from './Logotype.svg';
 
-export { default as HamburgerSvg } from './Hamburger.svg';
-export { default as CloseSvg } from './Close.svg';
-export { default as UploadSvg } from './Upload.svg';
-export { default as ImageSvg } from './Image.svg';
+export { default as HamburgerSvg } from './hamburger.svg';
+export { default as CloseSvg } from './close.svg';
+export { default as UploadSvg } from './upload.svg';
+export { default as ImageSvg } from './image.svg';
 export { default as XSvg } from './X.svg';
